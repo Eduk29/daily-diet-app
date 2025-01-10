f6e0ed8c5e6a5eaf44d8d07b94e450359200e592
Eduardo Marques <edu.temarques@gmail.com>
2025-01-10 13:24:53 -0300

    feat: impl. meals summarize by user

--- daily-diet-app-api/src/meals/meals.controller.ts
+++ daily-diet-app-api/src/meals/meals.controller.ts
@@ -174,4 +174,103 @@ export const mealsController = async (app: FastifyInstance) => {
             })
         }
     })
+
+    // TODO: Implement the endpoint to delete a meal by ID
+    app.delete('/:id/delete', { preHandler: extractSessionIdFromCookie }, async (request: FastifyRequest, response: FastifyReply) => {
+        // TODO: Validate meal ID from request parameters
+        const getMealIdParamSchema = z.object({
+            id: z.string().uuid()
+        })
+
+        try {
+            // TODO: Extract meal ID from request parameters
+            const { id } = getMealIdParamSchema.parse(request.params);
+
+            // TODO: Extract user ID from request
+            const userId = request.user?.id;
+
+            // TODO: Load meal from the database by ID and user ID
+            const meal = await knex('meals').where({ id }).select().first();
+
+            // TODO: Validate if the meal exists and belongs to the user
+            if (meal && meal.user_id === userId) {
+                // TODO: If the meal exists and belongs to the user, delete the meal
+                await knex('meals').where({ id }).andWhere({ user_id: userId }).delete().catch((error) => {
+                    console.error('Error deleting meal', error);
+                    return response.status(500).send({
+                        title: 'Internal Server Error',
+                        message: 'Error deleting meal, please try again later!'
+                    })
+                });
+
+                // TODO: Return success response with no content
+                return response.status(204).send({});
+
+            } else {
+                // TODO: If the meal does not exist or does not belong to the user, return error response
+                return response.status(403).send({ message: "You don't have permission to delete this meal" });
+            }
+
+        } catch (error) {
+            console.error('Invalid request parameter', error);
+            return response.status(400).send({
+                title: 'Bad Request',
+                message: 'Invalid request parameter OR meal information, please provide meal ID AND meal information!'
+            })
+        }
+    })
+
+    // TODO: Implement the endpoint to summarize the meals created by the user
+    app.get('/summary', { preHandler: extractSessionIdFromCookie }, async (request: FastifyRequest, response: FastifyReply) => {
+        // TODO: Extract user ID from request
+        const userId = request.user?.id;
+
+        // TODO: Load all meals from the database by the user
+        await knex('meals').where('user_id', userId).select().then((meals) => {
+            // TODO: Calculate the total number of meals created by the user
+            const totalMeals = meals.length;
+
+            // TODO: Calculate the total number of meals that are in the diet
+            const mealsOnDiet = _calculateMealsInDiet(meals);
+
+            // TODO: Calculate the total number of meals that are not in the diet
+            const mealsNotOnDiet = _calculateMealsNotInDiet(meals);
+
+            // TODO: Calculate the best sequence of meals in diet created by the user
+            const bestSequenceOfMealsInDiet = _calculateBestSequenceOfMealsInDiet(meals);
+
+            // TODO: Return success response with the summary information
+            return response.status(200).send({ totalMeals: meals.length, mealsOnDiet: mealsOnDiet.length, mealsNotOnDiet: mealsNotOnDiet.length, bestSequenceOfMealsInDiet });
+        }).catch((error) => {
+            console.error('Error loading meals', error);
+            return response.status(500).send({
+                title: 'Internal Server Error',
+                message: 'Error loading meals, please try again later!'
+            })
+        });
+    })
+
+    const _calculateMealsInDiet = (meals: any[]) => {
+        return meals.filter((meal) => meal.is_in_diet);
+    }
+
+    const _calculateMealsNotInDiet = (meals: any[]) => {
+        return meals.filter((meal) => !meal.is_in_diet);
+    }
+
+    const _calculateBestSequenceOfMealsInDiet = (meals: any[]) => {
+        const { onDietSequence } = meals.reduce(
+            (acc, meal) => {
+                acc.currentSequence = meal.is_in_diet ? acc.currentSequence + 1 : 0;
+
+                if (acc.currentSequence > acc.onDietSequence) {
+                    acc.onDietSequence = acc.currentSequence
+                }
+                return acc
+            },
+            { onDietSequence: 0, currentSequence: 0 },
+        )
+
+        return onDietSequence;
+    }
 }
